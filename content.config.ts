288cab8184a9cc6283abdddcd8046459564e7e2f
tsukiyama-3 <tsukiyama.mail@gmail.com>
2025-03-20 16:35:24 +0900

    fix: directory

--- content.config.ts
+++ content.config.ts
@@ -4,7 +4,7 @@ export default defineContentConfig({
   collections: {
     tech: defineCollection({
       type: 'page',
-      source: 'blog/tech/*.md',
+      source: 'tech/*.md',
       schema: z.object({
         title: z.string(),
         description: z.string(),
