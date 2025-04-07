24bd6e684ea21cd6bda0a1013b8f7590d2370a25
tsukiyama-3 <tsukiyama.mail@gmail.com>
2025-04-08 00:30:16 +0900

    fix: meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,26 @@ export default defineNuxtConfig({
     '~/components',
   ],
   devtools: { enabled: true },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'ja',
+      },
+      title: 'tsukiyama.blog',
+      meta: [
+        {
+          name: 'thumbnail',
+          content: 'https://res.cloudinary.com/dyoyv8djx/image/upload/v1744039369/tsukiyama-blog/tsukiyama.blog_uaoqwg.png',
+        },
+        { name: 'twitter:card', content: 'summary' },
+        { property: 'og:site_name', content: 'tsukiyama.blog' },
+        { property: 'og:title', content: 'tsukiyama.blog' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:url', content: 'https://tsukiyama.blog/' },
+        { property: 'og:image', content: 'https://res.cloudinary.com/dyoyv8djx/image/upload/v1744039369/tsukiyama-blog/tsukiyama.blog_uaoqwg.png' },
+      ],
+    },
+  },
   css: ['~/assets/css/tailwind.css'],
   content: {
     build: {
