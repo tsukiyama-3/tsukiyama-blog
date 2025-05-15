c60b10b5b3122a3197ff560f97e8c695c2cba329
tsukiyama-3 <tsukiyama.mail@gmail.com>
2025-05-15 22:29:28 +0900

    setup: sitemap

--- content.config.ts
+++ content.config.ts
@@ -1,4 +1,5 @@
 import { defineContentConfig, defineCollection, z } from '@nuxt/content'
+import { asSitemapCollection } from '@nuxtjs/sitemap/content'
 
 export default defineContentConfig({
   collections: {
@@ -16,5 +17,12 @@ export default defineContentConfig({
         updatedAt: z.date().optional(),
       }),
     }),
+    content: defineCollection(
+      // adds the robots frontmatter key to the collection
+      asSitemapCollection({
+        type: 'page',
+        source: '**/*.md',
+      }),
+    ),
   },
 })
