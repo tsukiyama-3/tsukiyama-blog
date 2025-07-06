bb1d231a7761805d1600cf8bb810b874925aa39b
tsukiyama-3 <tsukiyama.mail@gmail.com>
2025-07-06 14:25:18 +0900

    fix: demo browser

--- components/sample/GeoLocation.vue
+++ components/sample/GeoLocation.vue
@@ -10,11 +10,11 @@ const { position, getPosition } = useGeoLocation()
       <li>緯度：{{ position?.coords.latitude }}</li>
       <li>軽度：{{ position?.coords.longitude }}</li>
     </ul>
-    <button
-      class="bg-blue-600 w-fit block mx-auto text-white rounded-sm py-1 px-2 cursor-pointer"
+    <UButton
+      class="w-fit block mx-auto cursor-pointer"
       @click="getPosition"
     >
       位置情報を取得する
-    </button>
+    </UButton>
   </div>
 </template>
