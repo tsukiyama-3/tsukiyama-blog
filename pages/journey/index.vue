95213e37e0c9052342908b2fd193796a6e784dc2
tsukiyama-3 <tsukiyama.mail@gmail.com>
2025-07-07 19:05:42 +0900

    add: pages

---
+++ pages/journey/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import GlobeMaker from '~/components/globe/GlobeMaker.vue'
+</script>
+
+<template>
+  <UPage>
+    <div class="grid md:grid-cols-2">
+      <h1>Journey</h1>
+      <GlobeMaker />
+    </div>
+  </UPage>
+</template>
