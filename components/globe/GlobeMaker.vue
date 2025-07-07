3cb87e09bdd3cacff33e65e0638382902cb97310
tsukiyama-3 <tsukiyama.mail@gmail.com>
2025-07-07 20:17:29 +0900

    fix: param

--- components/globe/GlobeMaker.vue
+++ components/globe/GlobeMaker.vue
@@ -1,43 +1,79 @@
 <script setup lang="ts">
-import { Scene, PerspectiveCamera, AmbientLight, WebGLRenderer, SphereGeometry, MeshPhongMaterial, Mesh, TextureLoader } from 'three'
+import type { MeshPhongMaterial } from 'three'
+import { Scene, PerspectiveCamera, AmbientLight, Fog, PointLight, DirectionalLight, WebGLRenderer, Color } from 'three'
 import { OrbitControls } from 'three/examples/jsm/Addons.js'
+import countries from '~/public/json/globe-data-min.json'
 
 const container = ref<HTMLDivElement | null>(null)
 
-onMounted(() => {
+onMounted(async () => {
+  const { default: ThreeGlobe } = await import('three-globe')
   if (!container.value) return
 
   const scene = new Scene()
+  scene.add(new AmbientLight(0xbbbbbb, 0.3))
   scene.background = null
-  const camera = new PerspectiveCamera(
-    75,
-    container.value.clientWidth / container.value.clientHeight,
-    0.1,
-    1000,
-  )
-  camera.position.z = 2
+  const camera = new PerspectiveCamera()
+  camera.updateProjectionMatrix()
 
-  const texture = new TextureLoader().load('/8k_earth_daymap.jpg')
+  const dLight = new DirectionalLight(0xffffff, 0.8)
+  dLight.position.set(-600, 1600, 400)
+  camera.add(dLight)
+
+  const dLight1 = new DirectionalLight(0x7982f6, 1)
+  dLight1.position.set(-150, 400, 150)
+  camera.add(dLight1)
+
+  const dLight2 = new PointLight(0x8566cc, 0.8)
+  dLight2.position.set(-150, 400, 150)
+  camera.add(dLight2)
+
+  camera.position.z = 300
+  camera.position.x = 0
+  camera.position.y = 0
+
+  scene.add(camera)
+
+  scene.fog = new Fog(0x535ef3, 400, 2000)
+
+  const Globe = new ThreeGlobe({
+    waitForGlobeReady: true,
+    animateIn: true,
+  })
+    .hexPolygonsData(countries.features)
+    .hexPolygonResolution(3)
+    .hexPolygonMargin(0.3)
+    .showAtmosphere(true)
+    .atmosphereColor('#7868e6')
+    .atmosphereAltitude(0.25)
+    .hexPolygonColor((e: any) => {
+      if (
+        ['USA'].includes(
+          e.properties.ISO_A3,
+        )
+      ) {
+        return 'rgba(255,255,255, 1)'
+      }
+      else return 'rgba(255,255,255, 0.5)'
+    })
+
+  const globeMaterial = Globe.globeMaterial() as MeshPhongMaterial
+  globeMaterial.color = new Color(0x4b6aff) // 4b6aff
+  globeMaterial.emissive = new Color(0xa16ae8) // a16ae8
+  globeMaterial.emissiveIntensity = 0.1
+  globeMaterial.shininess = 0.7
+  globeMaterial.map = null
+
+  scene.add(Globe)
 
   const renderer = new WebGLRenderer({ antialias: true, alpha: true })
   renderer.setSize(container.value.clientWidth, container.value.clientHeight)
   container.value.appendChild(renderer.domElement)
 
-  const geometry = new SphereGeometry(1, 64, 64)
-  const material = new MeshPhongMaterial({
-    map: texture,
-    specular: 0xdddddd,
-    shininess: 30,
-  })
-  const sphere = new Mesh(geometry, material)
-  scene.add(sphere)
-
-  const ambient = new AmbientLight(0xffffff, 1) // 明るさは1で充分
-  scene.add(ambient)
-
   const controls = new OrbitControls(camera, renderer.domElement)
   controls.enableDamping = true
   controls.enableZoom = false
+  controls.enablePan = false
 
   const animate = () => {
     requestAnimationFrame(animate)
