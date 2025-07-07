e382c16940bb0d1a0df0360b3505a55f48fcf86f
tsukiyama-3 <tsukiyama.mail@gmail.com>
2025-07-07 22:08:42 +0900

    add: dom

--- components/globe/GlobeMaker.vue
+++ components/globe/GlobeMaker.vue
@@ -2,6 +2,7 @@
 import type { MeshPhongMaterial } from 'three'
 import { Scene, PerspectiveCamera, AmbientLight, Fog, PointLight, DirectionalLight, WebGLRenderer, Color } from 'three'
 import { OrbitControls } from 'three/examples/jsm/Addons.js'
+import { CSS2DRenderer } from 'three/addons/renderers/CSS2DRenderer.js'
 import countries from '~/public/json/globe-data-min.json'
 
 const container = ref<HTMLDivElement | null>(null)
@@ -36,6 +37,18 @@ onMounted(async () => {
 
   scene.fog = new Fog(0x535ef3, 400, 2000)
 
+  const markerSvg = `<svg viewBox="-4 0 36 36">
+      <path fill="currentColor" d="M14,0 C21.732,0 28,5.641 28,12.6 C28,23.963 14,36 14,36 C14,36 0,24.064 0,12.6 C0,5.641 6.268,0 14,0 Z"></path>
+      <circle fill="black" cx="14" cy="14" r="7"></circle>
+    </svg>`
+  const N = 30
+  const gData = [...Array(N).keys()].map(() => ({
+    lat: (Math.random() - 0.5) * 180,
+    lng: (Math.random() - 0.5) * 360,
+    size: 7 + Math.random() * 30,
+    color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)],
+  }))
+
   const Globe = new ThreeGlobe({
     waitForGlobeReady: true,
     animateIn: true,
@@ -56,6 +69,16 @@ onMounted(async () => {
       }
       else return 'rgba(255,255,255, 0.5)'
     })
+    .htmlElementsData(gData)
+    .htmlElement((d) => {
+      const el = document.createElement('div')
+      el.innerHTML = markerSvg
+      el.style.color = d.color
+      el.style.width = `${d.size}px`
+      el.style.transition = 'opacity 250ms'
+      return el
+    })
+    .htmlElementVisibilityModifier((el, isVisible) => el.style.opacity = isVisible ? '1' : '0')
 
   const globeMaterial = Globe.globeMaterial() as MeshPhongMaterial
   globeMaterial.color = new Color(0x4b6aff) // 4b6aff
@@ -66,19 +89,34 @@ onMounted(async () => {
 
   scene.add(Globe)
 
-  const renderer = new WebGLRenderer({ antialias: true, alpha: true })
-  renderer.setSize(container.value.clientWidth, container.value.clientHeight)
-  container.value.appendChild(renderer.domElement)
+  Globe.setPointOfView(camera)
 
-  const controls = new OrbitControls(camera, renderer.domElement)
+  const renderers = [new WebGLRenderer({ antialias: true, alpha: true }), new CSS2DRenderer()]
+
+  renderers.forEach((r, idx) => {
+    r.setSize(container.value!.clientWidth, container.value!.clientHeight)
+    if (idx > 0) {
+    // WebGL の上に重ねる
+      r.domElement.style.position = 'absolute'
+      r.domElement.style.top = '0px'
+      r.domElement.style.pointerEvents = 'none'
+    }
+    container.value!.appendChild(r.domElement)
+  })
+
+  const controls = new OrbitControls(camera, renderers[0].domElement)
   controls.enableDamping = true
   controls.enableZoom = false
   controls.enablePan = false
 
+  controls.addEventListener('change', () => {
+    Globe.setPointOfView(camera)
+  })
+
   const animate = () => {
     requestAnimationFrame(animate)
     controls.update()
-    renderer.render(scene, camera)
+    renderers.forEach(r => r.render(scene, camera))
   }
 
   animate()
@@ -88,7 +126,7 @@ onMounted(async () => {
     if (!container.value) return
     camera.aspect = container.value.clientWidth / container.value.clientHeight
     camera.updateProjectionMatrix()
-    renderer.setSize(container.value.clientWidth, container.value.clientHeight)
+    renderers[0].setSize(container.value.clientWidth, container.value.clientHeight)
   })
 })
 </script>
@@ -96,6 +134,6 @@ onMounted(async () => {
 <template>
   <div
     ref="container"
-    class="w-full aspect-square"
+    class="w-full aspect-square relative"
   />
 </template>
