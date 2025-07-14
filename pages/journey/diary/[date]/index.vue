7938c8558fbd7e47ba6c2bade53e1fbc19341d5f
tsukiyama-3 <tsukiyama.mail@gmail.com>
2025-07-14 22:08:08 +0900

    display

--- pages/journey/diary/[date]/index.vue
+++ pages/journey/diary/[date]/index.vue
@@ -97,6 +97,7 @@ const { convertIcon, convertText } = useWeatherIcon()
     class="dark:text-highlighted"
   >
     <template #left>
+      <p>{{ config.public.googleMaps.apiKey }}</p>
       <UPageHeader
         headline="Day 1"
         :title="article.title"
