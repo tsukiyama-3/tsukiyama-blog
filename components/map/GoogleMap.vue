68a5b5d5e9d421adcccece57c12152d946f2e93a
tsukiyama-3 <tsukiyama.mail@gmail.com>
2025-07-20 17:00:09 +0900

    fix: component

--- components/map/GoogleMap.vue
+++ components/map/GoogleMap.vue
@@ -17,17 +17,12 @@ const { onLoaded } = useScriptGoogleMaps({
 onLoaded(async (instance) => {
   const maps = await instance.maps
   const { Map } = await maps.importLibrary('maps') as google.maps.MapsLibrary
-
-  // 'marker' ライブラリから AdvancedMarkerElement を取り出し、型を明示
   const { AdvancedMarkerElement } = await maps.importLibrary('marker') as google.maps.MarkerLibrary
 
   const map = new Map(mapRef.value!, {
     center: props.position,
     zoom: 16,
-    tilt: 90,
-    heading: 0,
-    // mapId: '4dd6c17f0750a29aa8d88c90', // ベクター
-    mapId: '4dd6c17f0750a29a89cda4c8', // ラスター
+    mapId: config.public.googleMaps.mapId.raster,
   })
 
   if (props.enableMarker) {
