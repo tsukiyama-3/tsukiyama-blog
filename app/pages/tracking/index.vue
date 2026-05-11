aa6bf68372f684d4263f66bca6bdb906cd33e1f5
tsukiyama-3 <tsukiyama.mail@gmail.com>
2026-05-11 10:15:44 +0900

    test

---
+++ app/pages/tracking/index.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import { useHead, useScriptXPixel } from '#imports'
+
+useHead({
+  title: 'X Pixel',
+})
+
+// composables return the underlying api as a proxy object and the script state
+const { status, proxy } = useScriptXPixel({ id: 'ol7lz' })
+
+function triggerEvent() {
+  proxy.twq('event', 'tw-ol7lz-ol7mb', {
+    value: 123,
+  })
+}
+</script>
+
+<template>
+  <div>
+    <ClientOnly>
+      <div>
+        status: {{ status }}
+      </div>
+      <UButton @click="triggerEvent">
+        Trigger Event
+      </UButton>
+    </ClientOnly>
+  </div>
+</template>
